d86698c539c5e9dadffd96cb9a67ac1b3d760b15
--add <victor_ferreira@live.com.ar>
2024-10-27 17:24:31 -0300

    termine programa

--- perfil.js
+++ perfil.js
@@ -1,9 +1,28 @@
-let perfil = administrador;
-// let perfilB = asistente;
-// let perfilC = invitado;
+
+let perfil = "asistente";
 
 
-console.log(perfil);
+
+if (perfil === "administrador") {
+    console.log("Usted tiene todos los privilegios de uso del sistema");
+}
+else if (perfil === "") {
+    console.log("Debe especificar el perfil del usuario");
+}
+
+else if (perfil === "asistente") {
+    console.log("Usted sólo tiene permisos de registrar,modificar y consultar datos");
+}
+else if (perfil === "invitado") {
+    console.log("Usted sólo tiene permisos de consultar datos");
+}
+else {
+    console.log("Debe especificar un perfil válido");
+
+}
+
+
+
 
 
 
