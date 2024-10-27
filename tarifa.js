51940165bf8c8097e401c785fb49b9d7c5f0d029
--add <victor_ferreira@live.com.ar>
2024-10-27 18:41:12 -0300

    ternario

---
+++ tarifa.js
@@ -0,0 +1,9 @@
+let pagoMes = 30000;
+let consumoKWH = 350;
+let recargo = 20*pagoMes/100+pagoMes;
+(consumoKWH > 300)? console.log(recargo): console.log("Ustede mantiene el subsidio,y pagara"+" "+"$"+pagoMes);
+
+  
+ 
+
+console.log("Debido a que su hogar tuvo un consumo de 450kwh, en base al ajuste tarifario,cumplimos con informarle que se ha ajustado el total a pagar, que será de"+" "+"$"+recargo);
